3583bd11a5cce727858350bc6fa91c171f3c953c
Robowizx <omkariyer99@gmail.com>
2020-06-28 11:30:27 +0530

    merge fixes

--- Backend/API/server.js
+++ Backend/API/server.js
@@ -1,10 +1,10 @@
 //code for serving API goes here.
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
-const routes = require('./routes/exp_imp_index');
-const route = require('./routes/res_dyn');
-const routeh = require('./routes/res_his');
+const PORT = process.env.PORT || 4000;
+const exp_chart = require('./routes/exp_imp_index');
+const res_dyn = require('./routes/res_dyn');
+const res_stk = require('./routes/res_his');
 const db = require('./db')
 const body_parser = require("body-parser");
 
@@ -15,6 +15,8 @@ app.set('view cache',true);
 app.use(body_parser.json());
 app.use(express.static('./Public'));
 app.use(exp_chart);
+app.use(res_dyn);
+app.use(res_stk);
 
 
 db.connect((err)=>{
