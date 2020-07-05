020eadc9a46e5c5eb301b43efa02149aebc8f2ef
Robowizx <omkariyer99@gmail.com>
2020-07-05 23:44:48 +0530

    added indian timezone timestamp to logger

--- Backend/API/logger.js
+++ Backend/API/logger.js
@@ -1,4 +1,5 @@
-//importing winston module
+//importing winston and moment module
+const moment = require('moment');
 const {createLogger,format,transports} = require('winston');
 const {combine,timestamp,printf,colorize} = format;
 
@@ -7,16 +8,30 @@ const logFormat = printf(({ level, message, timestamp }) => {
     return `${timestamp}@${level}: ${message}`;
   });
 
+//timezone option for logger
+  const appendTimestamp = format((info, opts) => {
+  if(opts.tz)
+    info.timestamp = moment().tz(opts.tz).format();
+    return info;
+});  
+
 //setting options for logger 
   const options = {
   //settings for logging on console  
   console:{
-    format: combine(timestamp(),colorize({level:true,colors:{error:'red',warn:'yellow',info:'blue'}}),logFormat)
+    format: combine(timestamp(),
+                    appendTimestamp({tz:'Asia/Kolkata'}),
+                    colorize({level:true,colors:{error:'red',warn:'yellow',info:'blue'}}),
+                    logFormat
+                   )
   },
   //settings for logging to a file
   record:{
     filename:'./server.log',
-    format: combine(timestamp(),logFormat)
+    format: combine(timestamp(),
+                    appendTimestamp({tz:'Asia/Kolkata'}),
+                    logFormat
+                   )
   }
 }  
   
