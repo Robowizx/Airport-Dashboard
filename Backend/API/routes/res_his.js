0a4d62f366245c066ad8e9e9a09d9ff792e9d777
Robowizx <omkariyer99@gmail.com>
2020-07-05 23:54:13 +0530

    minor logging fix

--- Backend/API/routes/res_his.js
+++ Backend/API/routes/res_his.js
@@ -14,7 +14,7 @@ const db = require("../db");
 //res_his chart route code
 router.get("/:air/resh/:type", (req, res) => {
   const type = req.params.type +".responses";
-  serverLog.info(`REQUESTED Response histogram chart with Airport=${req.params.air},`+
+  serverLog.info(`REQUESTED Response histogram chart with Airport=${req.params.air}, `+
                  `Section=${req.params.type}, `+
                  `Date=${req.query.date}, `+
                  `Type=${req.query.dev}`
