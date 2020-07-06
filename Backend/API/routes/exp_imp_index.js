3fcd33a38043d59890632350470cf167e8792ea8
Prathik-Somanath <prathik.s1999@gmail.com>
2020-07-06 12:49:49 +0530

    conflicts

--- Backend/API/routes/exp_imp_index.js
+++ Backend/API/routes/exp_imp_index.js
@@ -16,19 +16,12 @@ router.get('/:air/exp/:sec',(req,res)=>{
 
     let db_query = { _id:0};
     db_query[`${req.params.sec}.responses`] = 1;
-<<<<<<< HEAD
-    let expdata=[];
-    let impdata = [];
-    let area = [];
-    let series = [];
-=======
     serverLog.info(`REQUESTED Exp/Imp chart with Airport=${req.params.air}, `+
                    `Section=${req.params.sec}, `+
                    `Date=${req.query.date}, `+
                    `Type=${req.query.type}`
                   );
 
->>>>>>> 0a4d62f366245c066ad8e9e9a09d9ff792e9d777
     db.getDB().collection(req.params.air).find({date: req.query.date, type: req.query.type}).project(db_query).toArray((err,documents)=>{
         if(err){
             serverLog.error(`Exp/Imp chart DATABASE ERROR with Airport=${req.params.air}, `+
