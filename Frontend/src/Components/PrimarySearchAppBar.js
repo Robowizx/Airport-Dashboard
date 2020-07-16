4151ccff803ac089691e2458c1e8a87800f8ebc4
Maithriudupa <57523022+Maithriudupa@users.noreply.github.com>
2020-07-16 23:03:28 +0530

    sample of device page

--- Frontend/src/Components/PrimarySearchAppBar.js
+++ Frontend/src/Components/PrimarySearchAppBar.js
@@ -10,7 +10,7 @@ import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import SimpleTabs from "./trial.js"
+import SimpleTabs from "./Device_page.js"
 const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
