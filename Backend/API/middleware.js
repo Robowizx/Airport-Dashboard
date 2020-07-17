9adeb24ca8e5e3879478eaebbdd4d3c0c8a3cb65
Prathik-Somanath <prathik.s1999@gmail.com>
2020-07-17 16:19:00 +0530

    changes made to middleware.js

--- Backend/API/middleware.js
+++ Backend/API/middleware.js
@@ -7,36 +7,16 @@ export const bToken = async ()=>{
     let cpayload = {
         issuer:"localhost",
         audience: "localhost:4000",
-        domain: "localhost"
       };
     let BToken = jwt.sign(cpayload,key,{expiresIn:"30m"});
     const response = await fetch('/auth',{
-        method: 'POST',
+        method: 'GET',
         headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
         'host': 'localhost',
         'Authorization': "Bearer " + BToken
         }
     });
     const { AToken } = await response.json();
-    localStorage.setItem('token', AToken);
+    return AToken;
 }
 
-export const checkToken = (req, res, next) => {
-  let token = localStorage.getItem('token'); 
-  if (token) {
-    jwt.verify(token, config.secret, (err, decoded) => {
-      if (err) {
-        serverLog.warn(`Authentication TOKEN Err: ${err}`)
-        res.status(401).send("error 401 Unauthorized");
-      } else {
-        req.decoded = decoded;
-        next();
-      }
-    });
-  } else {
-    serverLog.warn(`Authentication DATA NOT FOUND`);
-    res.status(401).send("error 401 Unauthorized");
-  }
-};
