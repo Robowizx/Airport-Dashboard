33f0277a8fb1d542bba0e0f1eca363f6437fc364
Prathik-Somanath <prathik.s1999@gmail.com>
2020-07-17 14:19:05 +0530

    changes made to middleware.js

--- Backend/API/middleware.js
+++ Backend/API/middleware.js
@@ -3,13 +3,13 @@ const serverLog = require('./logger');
 const jwt = require('jsonwebtoken');
 const key = "icAKI50fOLKCwjIzOZAnrD0R9QE3C5ik";
 
-let cpayload = {
-    issuer:"localhost",
-    audience: "localhost:4000",
-    domain: "localhost"
-  };
-let BToken = jwt.sign(cpayload,key,{expiresIn:"30m"});
 export const bToken = async ()=>{
+    let cpayload = {
+        issuer:"localhost",
+        audience: "localhost:4000",
+        domain: "localhost"
+      };
+    let BToken = jwt.sign(cpayload,key,{expiresIn:"30m"});
     const response = await fetch('/auth',{
         method: 'POST',
         headers: {
